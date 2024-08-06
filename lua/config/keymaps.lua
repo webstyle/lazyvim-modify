c6b3be6661f2fd219944e8a2abbeb03c3b760b92
Farrukh <mfarrukhjon@gmail.com>
2024-08-06 22:51:16 +0500

    modified

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("n", "gb", "<C-O>", { desc = "Go to bakc" })
+
+keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" }) -- show lsp implementations
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+  "williamboman/mason.nvim",
+  cmd = "Mason",
+  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+  build = ":MasonUpdate",
+  opts_extend = { "ensure_installed" },
+  opts = {
+    ensure_installed = {
+      "shfmt",
+      "stylua",
+      "typescript-language-server",
+    },
+  },
+}
