f59e3cd845d4017e1331e13cf6f6cc5e03972796
Farrukh <mfarrukhjon@gmail.com>
2024-08-22 18:36:03 +0500

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,9 @@ keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Show LSP d
 keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Show LSP type definitions" }) -- show lsp type definitions
 
 keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Open toggle term" }) --  move current buffer to new tab
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
