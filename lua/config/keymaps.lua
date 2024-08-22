eca13fcf8a0c2657c72a6473ca49666446f75226
Farrukh <mfarrukhjon@gmail.com>
2024-08-22 18:31:34 +0500

    commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,7 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("n", "gb", "<C-O>", { desc = "Go to bakc" })
 
 keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" }) -- show lsp implementations
+
+keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Show LSP definitions" }) -- show lsp definitions
+
+keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Show LSP type definitions" }) -- show lsp type definitions
